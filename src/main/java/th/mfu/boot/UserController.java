260c9354b6ede76f53a97072c6b4559e6a065073
6731503119Waritpon <6731503119@lamduan.mfu.ac.th>
2025-09-01 15:24:13 +0700

    04-boot-6731503119Waritpon v1

--- src/main/java/th/mfu/boot/UserController.java
+++ src/main/java/th/mfu/boot/UserController.java
@@ -1,3 +1,4 @@
+// src/main/java/th/mfu/boot/UserController.java
 package th.mfu.boot;
 
 import java.util.Collection;
@@ -6,31 +7,36 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    public static Map<String, User> users = new HashMap<String, User>();
+    public static Map<String, User> users = new HashMap<>();
 
-   
-    public ResponseEntity<String> registerUser( User user) {
-      //TODO
-      return null;
+    @PostMapping("/users")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        if (user == null || user.getUsername() == null || user.getUsername().isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("username is required");
+        }
+        if (users.containsKey(user.getUsername())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists");
+        }
+        users.put(user.getUsername(), user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User created");
     }
 
+    @GetMapping("/users")
     public ResponseEntity<Collection<User>> list() {
-        //TODO
-        return null;
+        return ResponseEntity.ok(users.values());
     }
 
-    public ResponseEntity<User> getUser(String username) {
-        //TODO
-        return null;
+    @GetMapping("/users/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        User u = users.get(username);
+        if (u == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(u);
     }
-    
 }
